ca2d04f0e501c9157980eaa622ff6b2a4a6a223c
Louie Heaton <louie@hatcha.co.uk>
2023-05-28 21:31:24 +0100

    fix: remove rewrite

--- next.config.js
+++ next.config.js
@@ -14,16 +14,8 @@ const nextConfig = {
     },
     env: {
         AWS_COGNITO_REGION: process.env.AWS_COGNITO_REGION,
-        AWS_COGNITO_CLIENTID: process.env.AWS_COGNITO_CLIENTID,
-        AWS_COGNITO_POOLID: process.env.AWS_COGNITO_POOLID,
-    },
-    async rewrites() {
-        return [
-            {
-                source: '/',
-                destination: '/homepage',
-            },
-        ];
+        AWS_COGNITO_POOL_ID: process.env.AWS_COGNITO_POOL_ID,
+        AWS_COGNITO_APP_CLIENT_ID: process.env.AWS_COGNITO_APP_CLIENT_ID,
     },
 };
 
